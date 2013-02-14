f315ca98d536231eb75eb9ab2cb5e86e47e26ab4
StuFF mc <mc@stuffmc.com>
2013-02-14 16:23:51 +0100

    Very Very (Very) basic sample to play around with a Custom Table View Cell in a Static Table View
    Made this tiny project because I'm having troubles with a "real"
    project.

---
+++ StaticCustomCell/MCTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  MCTableViewCell.h
+//  StaticCustomCell
+//
+//  Created by Manuel "StuFF mc" Carrasco Molina on 14.02.13.
+//  Copyright (c) 2013 Pomcast.biz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MCTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *coloredLabel;
+
+@end
---
+++ StaticCustomCell/MCTableViewCell.m
@@ -0,0 +1,13 @@
+//
+//  MCTableViewCell.m
+//  StaticCustomCell
+//
+//  Created by Manuel "StuFF mc" Carrasco Molina on 14.02.13.
+//  Copyright (c) 2013 Pomcast.biz. All rights reserved.
+//
+
+#import "MCTableViewCell.h"
+
+@implementation MCTableViewCell
+
+@end
